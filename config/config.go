f1cc3352f769b8b0899ea58a724428fbd18a0855
Karthikeya-Akhandam-Kisanlink <karthikeya@kisanlink.in>
2025-07-08 17:51:09 +0530

    [FIX]: updated the config.go file to create the DB url dynamically from .env

--- config/config.go
+++ config/config.go
@@ -1,8 +1,7 @@
-// File: config/config.go
-
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -11,22 +10,48 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB                *gorm.DB
+	AAAServiceBaseURL string
+	AAASecret         string
+)
 
-func Load() {
-	_ = godotenv.Load()
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, using system environment variables")
+	}
+
+	AAAServiceBaseURL = os.Getenv("AAA_SERVICE_URL")
+	AAASecret = os.Getenv("AAA_SECRET")
 }
 
 func InitDB() (*gorm.DB, error) {
-	Load()
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		dsn = "postgresql://postgres:12345678@localhost:5432/jb?sslmode=disable"
+	LoadEnv()
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("POSTGRESS_USER")
+	password := os.Getenv("POSTGRESS_PASS")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	// Check required variables
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" || sslmode == "" {
+		return nil, fmt.Errorf("missing one or more required DB environment variables")
 	}
+
+	// Construct the DSN
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode,
+	)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("failed to connect database: %v", err)
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
+
 	DB = db
 	return db, nil
 }
